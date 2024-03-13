aa8a23e63abeae936a95b75892019db90cba0292
mavrick <gouix10@gmail.com>
2024-03-13 10:11:24 +0100

    initial commit

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -3,56 +3,48 @@ import java.util.List;
 
 public class Backtracking {
     public static void main(String[] args) {
-        System.out.println(backtrack(16));
+        System.out.println(backtrack(17));
     }
-
     public static List<Integer> backtrack(int n) {
         if (TERM(n)) {
             return new ArrayList<>();
         }
-
         if (NONSOL(n)) {
             return null;
         }
-
-        List<Integer> rules = APPREGLES(n);
+        List<Integer> rules = APPREGLES();
         List<Integer> chemin = null;
-
+        int r = 0;
         while (chemin == null && !rules.isEmpty()) {
-            int r = rules.get(0);
+            r = rules.get(0);
             rules.remove(0);
-            int rd = r(n);
+            int rd = n-r;
+            System.out.println(rd);
             chemin = backtrack(rd);
         }
-
         if (chemin != null) {
-            //chemin.add(0, r);
+            chemin.add(0, r);
             return chemin;
         } else {
             return null;
         }
     }
-
     private static boolean TERM(int n) {
         if (n==0)
             return true;
         else
             return false;
     }
-
     private static boolean NONSOL(int n) {
         if (n<0)
             return true;
         else
             return false;
     }
-
-    private static List<Integer> APPREGLES(int n) {
+    private static List<Integer> APPREGLES() {
         return new ArrayList<>(List.of(5, 2, 1));
     }
-
     private static int r(int n) {
-
         return n;
     }
 }
